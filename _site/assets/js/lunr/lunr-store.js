7e389502190d2fa962174d0847b86f8ae5320693
jovis <xinapse-jovis>
2020-11-03 13:35:58 +0900

    initializing

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,8 +1,8 @@
 var store = [{
-        "title": "R&D팀에서 뭘 하려고 하는것 같은데...",
-        "excerpt":"일단은 우리 다함께 피카츄 송을 불러보자 초반부 자 이제 시작이야(내꿈을) 내꿈을 위한 여행(피카츄) 걱정 따윈없어(없어) 내친구랑 함께니까(피카피카) 중반부 처음시작은 어색할지도 몰라(몰라 내친구 피카츄) 날지켜 줄거라고 믿고 있어 누구라도 얕보다간 큰일나 언제나 어디서나(피카츄가 옆에 있어) 약할때나 강할때나 (피카츄가 옆에있어) 너와나 함께라면 우린 최고야 후반부 언제언제까지나 진실한 마음으로 언제언제까지나 그날을 위해 피카츄!...","categories": ["자 이제 시작이야"],
+        "title": "GAN이 무엇인가요?",
+        "excerpt":"이번 포스트에서는 GAN의 개념과 작동원리를 수식 및 코드를 동원해 설명해보려 합니다. GAN은 여러 분야에서 널리쓰이지만, 시각적인 이해를 돕고자 이미지를 예로 들어서 input부터 output까지 어떻게 동작하는지 알아보겠습니다. Generative Adversarial Network [버스 한대가 여러 대가 될 수 있는 사진] 제목에서도 알 수 있듯, GAN은 Generative, 생성모델입니다. 만약 버스에 대한 데이터를 가지고 있다면...","categories": ["Computer Vision"],
         "tags": [],
-        "url": "http://localhost:4000/%EC%9E%90%20%EC%9D%B4%EC%A0%9C%20%EC%8B%9C%EC%9E%91%EC%9D%B4%EC%95%BC/first_post/",
+        "url": "http://localhost:4000/computer%20vision/first_post/",
         "teaser": null
       },{
         "title": "네번 째 포스트를 한번 테스트 해보겠습니다.",
